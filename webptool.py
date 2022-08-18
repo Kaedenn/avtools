0f560be51ab833faa1e911e5ba8a347653c3b9e8
Kaedenn <kaedenn@gmail.com>
2022-08-17 19:43:38 -0800

    Add describe mode

--- webptool.py
+++ webptool.py
@@ -8,6 +8,7 @@ import argparse
 import logging
 import os
 import string
+import subprocess
 import sys
 
 import webp
@@ -18,6 +19,7 @@ logging.basicConfig(level=logging.INFO,
     format="%(module)s:%(lineno)s: %(levelname)s: %(message)s")
 logger = logging.getLogger(__name__)
 
+MODE_DESCRIBE = "describe-webp"
 MODE_CREATE = "create-webp"
 MODE_EXTRACT = "extract-webp"
 MODE_ENCODE = "encode-video"
@@ -49,6 +51,80 @@ def gather_inputs(path_arg, input_arg):
       results.append(path)
     else:
       logger.warning("Ignoring %r: path doesn't exist", path)
+  return results
+
+def webpmux_info(path):
+  "Run webpmux -info and sanitize the output"
+  args = ["webpmux", "-info", path]
+  logger.debug("Invoking %s", subprocess.list2cmdline(args))
+  out = subprocess.check_output(args).decode()
+  for line in out.splitlines():
+    if ":" not in line:
+      logger.warning("Can't parse line %r", line)
+    elif line.count(":") == 1:
+      lkey, lval = line.split(":")
+      yield lkey.strip(), lval.strip()
+    elif line.count(":") == 2:
+      for field in line.split("  "):
+        lkey, lval = field.split(":", 1)
+        yield lkey.strip(), lval.strip()
+    else:
+      logger.warning("Unable to parse line %r", line)
+
+def get_webp_info(path):
+  "Get information about the WebP file"
+  webpinfo = webpmux_info(path)
+  results = {
+    "size": (0, 0),
+    "features": [],
+    "bgcolor": 0,
+    "loops": 0,
+    "nframes": 0,
+    "frames": [],
+    "duration": 0
+  }
+  fheaders = []
+  for lkey, lval in webpinfo:
+    lkey = lkey.lower()
+    if not lval:
+      logger.warning("key %s has no value", lkey)
+    elif lkey == "canvas size":
+      cxsize, cysize = lval.split(" x ")
+      results["size"] = (int(cxsize), int(cysize))
+    elif lkey == "features present":
+      results["features"] = lval.split()
+    elif lkey == "background color":
+      if lval.startswith("0x"):
+        lval = lval[2:]
+      if lval:
+        results["bgcolor"] = int(lval, 16)
+      else:
+        logger.warning("failed to parse bgcolor %s", lval)
+    elif lkey == "loop count":
+      results["loops"] = int(lval)
+    elif lkey == "number of frames":
+      results["nframes"] = int(lval)
+    elif lkey == "no.":
+      fheaders = lval.split()
+    elif lkey.isdigit():
+      fvalues = lval.split()
+      if len(fvalues) != len(fheaders):
+        logger.warning("Inconsistent frame data %r", lval)
+        continue
+      finfo = {"num": int(lkey)}
+      for fvkey, fvval in zip(fheaders, fvalues):
+        if fvval.isdigit():
+          fvval = int(fvval)
+        finfo[fvkey] = fvval
+      results["duration"] += finfo["duration"]
+      results["frames"].append(finfo)
+    elif lkey == "size of the xmp metadata":
+      results["xmpsize"] = lval
+    else:
+      logger.warning("Failed to parse key %s value %r", lkey, lval)
+  if results["nframes"] != len(results["frames"]):
+    logger.warning("Inconsistent frame count (expect %d, got %d)",
+        results["nframes"], len(results["frames"]))
   return results
 
 def load_images(paths):
@@ -124,6 +200,15 @@ def write_video(images, output_path, size_wxh=None, fps=24, encoder='MP4V'):
     out.write(img)
   out.release()
 
+def describe_webp_file(paths):
+  "Display information about a WebP file"
+  for path in paths:
+    images = load_images([path])
+    pl = "{} frame{}".format(len(images), "" if len(images) == 1 else "s")
+    print("{}: {}".format(path, pl))
+    isize = images[0].getbbox() # assume all images have the same size
+    print("Size: {}x{}".format(isize[2], isize[3]))
+
 def create_webp_file(images, output_path, fps=None):
   "Create a WebP file from the given images"
   kwargs = {}
@@ -175,8 +260,10 @@ This is done if the output path ends in ".webp".
   ag = ap.add_argument_group()
   mg = ag.add_mutually_exclusive_group()
   mg.add_argument("-m", "--mode",
-      choices=(MODE_CREATE, MODE_EXTRACT),
+      choices=(MODE_DESCRIBE, MODE_CREATE, MODE_EXTRACT),
       help="explicitly configure execution mode rather than deducing")
+  mg.add_argument("-d", "--describe", action="store_const",
+      dest="mode", const=MODE_DESCRIBE, help="Shorthand for --mode=%(const)s")
   mg.add_argument("-c", "--create", action="store_const",
       dest="mode", const=MODE_CREATE, help="Shorthand for --mode=%(const)s")
   mg.add_argument("-e", "--extract", action="store_const",
@@ -203,16 +290,40 @@ This is done if the output path ends in ".webp".
     ap.error("Nothing to process; exiting")
   logger.debug("Scanning %d file(s)", len(paths))
 
-  images = load_images(paths)
   mode = args.mode
   if mode is None:
     mode = deduce_mode(paths, args.output)
-  
-  if mode == MODE_CREATE:
+
+  if mode == MODE_DESCRIBE:
+    describe_webp_file(paths)
+    for path in paths:
+      vinfo = get_webp_info(path)
+      logger.debug(vinfo)
+      print("Features: {}".format(" ".join(vinfo["features"])))
+      print("Background: {:08x}".format(vinfo["bgcolor"]))
+      nloops = "infinite" if vinfo["loops"] == 0 else str(vinfo["loops"])
+      print("Loops: {}".format(nloops))
+      if vinfo["duration"] > 0:
+        fps = len(vinfo["frames"]) / (vinfo["duration"] / 1000)
+        print("Duration: {}ms (~{:.02f}fps)".format(vinfo["duration"], fps))
+      else:
+        print("Duration: unknown")
+      print("Frames: {}".format(len(vinfo["frames"])))
+      for frame in vinfo["frames"]:
+        print(("  {num:3d} {width}x{height}"
+               " alpha={alpha:3s}"
+               " offset=({x_offset},{y_offset})"
+               " {duration:3d}ms"
+               " blend={blend:3s}"
+               " {image_size}b"
+               " {compression}").format(**frame))
+  elif mode == MODE_CREATE:
+    images = load_images(paths)
     opath = deduce_create_output(args.output)
     logger.info("Creating WebP file {}".format(opath))
     create_webp_file(images, opath, fps=args.fps)
   elif mode == MODE_EXTRACT:
+    images = load_images(paths)
     oformat = deduce_extract_output(args.output)
     logger.info("Extracting WebP frames to {}".format(oformat))
     extract_webp_file(images, oformat)
